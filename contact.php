4ce8d358456c1dab74a42a6b13e0e348f8f431bd
Tyler Searle <tyler@weareflagship.com>
2016-11-23 16:49:34 -0700

    Fixed contact form to redirect to index.html page

--- contact.php
+++ contact.php
@@ -1,43 +1,21 @@
-
 <?php
-require_once 'vendor/autoload.php';
-
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-
-
-
-$mail = new PHPMailer;
-//Enable SMTP debugging.
-$mail->SMTPDebug = 3;
-//Set PHPMailer to use SMTP.
-$mail->isSMTP();
-//Set SMTP host name
-$mail->Host = 'smtp.gmail.com';
-//Set this to true if SMTP host requires authentication to send email
-$mail->SMTPAuth = true;
-//Provide username and password
-$mail->Username = 'tyler@weareflagship.com';
-$mail->Password = 'Tylsea45@';
-//If SMTP requires TLS encryption then set it
-$mail->SMTPSecure = 'tsl';
-//Set TCP port to connect to
-$mail->Port = 587;
-
-$mail->From = 'noreply@northfortycreative.com';
-$mail->FromName = 'North Forty Creative';
-
-$mail->addAddress($email);
-
-$mail->isHTML(true);
-
-$mail->Subject = 'New Client Inquiry';
-$mail->Body = 'Hello, we have a new client that we should contact, here is the contact information: <br />Name:' . $name .  '<br />Phone Number : ' . $phone . '<br />Message : ' . $message . ' <br />Thanks ';
-
-
-if(!$mail->send()) {
- return 'Mail error: '.$mail->ErrorInfo;
-}else{
-    return true;
+if($success) {
+	header('location:index.html');
 }
+else {
+	header('location:index.html');
+}
+
+    $name = @trim(stripslashes($_POST['name']));
+    $email = @trim(stripslashes($_POST['email']));
+    $message = @trim(stripslashes($_POST['message']));
+
+    $email_from = $email;
+    $email_to = 't.m.searle@gmail.com';//replace with your email
+
+    $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Message: ' . $message;
+
+    $success = @mail($email_to, $subject, $body, 'From: <'.$email_from.'>');
+
+    echo json_encode($status);
+    die;
