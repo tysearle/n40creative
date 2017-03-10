9201910d85648d8889ec0a4c360436d03f597e6c
Tyler Searle <tyler@weareflagship.com>
2017-03-10 16:46:01 -0700

    new changes

--- contact.php
+++ contact.php
@@ -11,7 +11,7 @@ else {
     $message = @trim(stripslashes($_POST['message']));
 
     $email_from = $email;
-    $email_to = 't.m.searle@gmail.com';//replace with your email
+    $email_to = 'tyler@n40creative.com';//replace with your email
 
     $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Message: ' . $message;
 
